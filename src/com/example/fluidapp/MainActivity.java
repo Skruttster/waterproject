574c0a7af0d500e402de90db4bdcdf80ce004c92
Emil Sunesson <sunesson.emil@gmail.com>
2013-09-27 13:24:49 +0200

    Testing with http.

--- src/com/example/fluidapp/MainActivity.java
+++ src/com/example/fluidapp/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.fluidapp;
 
+import java.io.IOException;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 public class MainActivity extends Activity {
 
@@ -20,4 +27,27 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void getLocation()
+    {
+    	String retJSON = "";
+    	String url = "http://api.openweathermap.org/data/2.1/find/city?lat=56.161416&lon=15.583822&cnt=1";
+    	
+    	    DefaultHttpClient httpClient = new DefaultHttpClient();
+    	    HttpGet httpGet = new HttpGet(url);
+
+    	    HttpResponse httpResponse;
+			try {
+				httpResponse = httpClient.execute(httpGet);
+				retJSON = httpResponse.toString();
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	    
+    	System.out.println("derp");
+    	
+    }
 }
