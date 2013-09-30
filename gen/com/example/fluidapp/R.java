17c8e089bc42f2ab183bfcf18ddda0018d938f0b
rehnen <rehnen@rehnen-HP-ENVY-14-Notebook-PC>
2013-09-30 03:03:00 +0200

    Lade till GUI mockups nu. Har lärt mig hur man arbetar med skiten. Kommer ha funktionallitet bunden till knapparna före tisdag, så vi kan jobba effektivare.

--- gen/com/example/fluidapp/R.java
+++ gen/com/example/fluidapp/R.java
@@ -19,26 +19,79 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int font_size=0x7f040002;
+        public static final int font_size_btn=0x7f040003;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int button1=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f080012;
+        public static final int btn=0x7f080000;
+        public static final int btnClear=0x7f080003;
+        public static final int btnCreatedBy=0x7f080001;
+        public static final int btnInfo=0x7f080005;
+        public static final int btnSettings=0x7f080004;
+        public static final int button1=0x7f080009;
+        public static final int editText1=0x7f080010;
+        public static final int editText2=0x7f08000e;
+        public static final int editText3=0x7f08000c;
+        public static final int editText4=0x7f08000a;
+        public static final int radioGroup1=0x7f080006;
+        public static final int rbWoman=0x7f080007;
+        public static final int rmMan=0x7f080008;
+        public static final int textView1=0x7f080011;
+        public static final int textView2=0x7f08000f;
+        public static final int textView3=0x7f08000d;
+        public static final int textView4=0x7f08000b;
+        public static final int txtVMessages=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_about=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_settings=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int about=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int settings=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int bthGoToAbout=0x7f050006;
+        public static final int bthGoToSettings=0x7f050005;
+        /**  Start page 
+ Button texts 
+         */
+        public static final int btnClearMessages=0x7f050004;
+        public static final int btn_about_app=0x7f050015;
+        public static final int btn_created_by=0x7f050014;
         public static final int hello_world=0x7f050002;
+        /**  Raio buttons 
+         */
+        public static final int rb_man=0x7f05000d;
+        public static final int rb_woman=0x7f05000e;
         public static final int send_request=0x7f050003;
+        /**  About page 
+         */
+        public static final int title_activity_about=0x7f050013;
+        public static final int title_activity_settings=0x7f050016;
+        public static final int title_age=0x7f05000b;
+        /**  Settings Page 
+ Titles 
+         */
+        public static final int title_gender=0x7f050008;
+        public static final int title_height=0x7f050009;
+        public static final int title_medicine=0x7f05000c;
+        public static final int title_weitght=0x7f05000a;
+        public static final int txb_age=0x7f050011;
+        public static final int txb_debicine=0x7f050012;
+        public static final int txtVDefaultMassage=0x7f050007;
+        /**  Textboxes 
+         */
+        public static final int txtb_height=0x7f05000f;
+        public static final int txtb_weight=0x7f050010;
     }
     public static final class style {
         /** 
